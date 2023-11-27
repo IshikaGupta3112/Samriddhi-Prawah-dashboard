634d17e5dcea4c540afb28fd3e8e7cfa8eaf3be5
Ishika Gupta <apple@apples-MacBook-Air-2.local>
2023-11-27 11:42:10 +0530

    changed base URL

--- src/redux/base.jsx
+++ src/redux/base.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 export default axios.create({
   // baseURL:"http://34.228.115.7:8080/"
-  baseURL: "https://sampaw.devalan.tech/api/",
+  // baseURL: "https://sampaw.devalan.tech/api/",
+  baseURL: "https://sempaw.onrender.com/api/",
 });
